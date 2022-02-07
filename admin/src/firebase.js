ee6e8a94890bd148c0038b27abe45c7ce2b45a0c
Abhishek Kumar <abhishek022kk@gmail.com>
2022-02-07 19:51:11 +0530

    updated the admin app

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDeZ19ueliyROVZ7s_PXxnxJNAfeqkSckQ',
+  authDomain: 'ecommerce-shop-22aa6.firebaseapp.com',
+  projectId: 'ecommerce-shop-22aa6',
+  storageBucket: 'ecommerce-shop-22aa6.appspot.com',
+  messagingSenderId: '581508672733',
+  appId: '1:581508672733:web:aa00e541841f2f1d823f34',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
